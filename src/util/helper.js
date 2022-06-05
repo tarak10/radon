c8ce4692c1e2a1cac3ec1b0a87527c76723b5c4d
tarak10 <tarkeshwarmahatodaviet@gmail.com>
2022-06-05 13:48:47 +0530

    done

--- src/util/helper.js
+++ src/util/helper.js
@@ -1,19 +1,19 @@
-const printDate = function() {
-    let currentDate = new Date()
-    console.log(currentDate)    
-}
+// const printDate = function() {
+//     let currentDate = new Date()
+//     console.log(currentDate)    
+// }
 
-const printMonth = function() {
-    let currentdate = new Date()
-    let currentMonth = currentdate.getMonth() + 1
-    console.log('The current month is '+currentMonth)
-}
+// const printMonth = function() {
+//     let currentdate = new Date()
+//     let currentMonth = currentdate.getMonth() + 1
+//     console.log('The current month is '+currentMonth)
+// }
 
-const getBatchInfo = function() {
-  let batchInformation = 'Radon, W3D4, the topic for today is Nodejs module system assignment discussion'
-  console.log(batchInformation)
-}
+// const getBatchInfo = function() {
+//   let batchInformation = 'my name is Tarkeshwar mahato i am radon '
+//   console.log(batchInformation)
+// }
 
-module.exports.printDate = printDate
-module.exports.getCurrentMonth = printMonth
-module.exports.getCohortData = getBatchInfo
+// module.exports.printDate = printDate
+// module.exports.getCurrentMonth = printMonth
+// module.exports.getCohortData = getBatchInfo
