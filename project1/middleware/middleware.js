f3ea8bea282135a85c8a30b4f4690f3327f65cd6
mdyousuf9007 <106535726+mdyousuf9007@users.noreply.github.com>
2022-06-22 11:47:42 +0530

    Update middleware.js

---
+++ project1/middleware/middleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+
+const authentication = async function(req, res, next) {
+    const token = req.headers['x-api-key']
+         if (!token) return res.status(400).send({ msg: "hey please provide token" })
+    const validToken = jwt.verify(token, 'projectOne')
+
+    if (!validToken) {
+        res.status(400).send({ status: false, msg: "the user is not found" })
+    }
+    //console.log(validToken)
+    req.body.tokenId = validToken._id
+    next()
+}
+module.exports.authentication = authentication;
