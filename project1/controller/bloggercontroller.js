03e84a1c1e7c65b18f5e6594766a890585bcac4a
irshadali740 <irshadali740@gmail.com>
2022-06-23 16:40:35 +0530

    allapi

--- project1/controller/bloggercontroller.js
+++ project1/controller/bloggercontroller.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const authorModel = require("../models/authorModel");
-const blogModel = require("../models/blogsModel")
+const blogsModel = require("../models/blogsModel")
+const moment = require('moment')
 
 //================================================ Creating Blogs ======================================================================// 
 
@@ -11,7 +12,7 @@ const createBlogs = async function (req, res) {
         let authorDetails = await authorModel.findById(authorId);
         if (!authorDetails)
             return res.status(404).send({ status: false, msg: "No such author exists" });
-        let savedData = await blogModel.create(blog);
+        let savedData = await blogsModel.create(blog);
         res.status(201).send({ status: true, data: savedData });
     }
     catch (error) {
@@ -23,9 +24,9 @@ const createBlogs = async function (req, res) {
 
 const getallblogs = async function (req, res) {
     try {
-        let allblogs = await Blogmodel.find({ isDeleted: false, isPublished: true })
+        let allblogs = await blogsModel.find({ isDeleted: false, isPublished: true })
         if (!allblogs) return res.status(404).send({ status: false, msg: "Blog does not exist" })
-        res.status(200).send({ status: true, data: allBlogs })
+        res.status(200).send({ status: true, data: allblogs })
     }
     catch (error) {
         res.status(500).send({ msg: "Error", error: error.message })
@@ -40,9 +41,10 @@ let getByFilter = async function (req, res) {
         let category = req.query.category
         let tag = req.query.tags
         let subcategory = req.query.subcategory
-        let allblogs = await blogModel.find({
+        let allblogs = await blogsModel.find({
             $or: [{ authorId: authorid }, { category: category }, { tags: tag }, { subcategory: subcategory }]
         })
+        if (allblogs!=0) return res.status(404).send({ status: false, msg: "Blog not found" })
         res.status(200).send({ status: true, data: allblogs })
     }
     catch (error) {
@@ -55,16 +57,16 @@ let getByFilter = async function (req, res) {
 const updateBlog = async function (req, res) {
     try {
         let blogId = req.params.blogId;
-        let blog = await blogModel.findById(blogId);
+        let blog = await blogsModel.findById(blogId);
         if (!blog) {
             return res.status(404).send({ status: false, msg: "No such blog exists" });
         }
         let blogData = req.body;
-        let updatedblog = await blogModel.findOneAndUpdate(
+        let updatedblog = await blogsModel.findOneAndUpdate(
             { _id: blogId, isDeleted: false },
             { $set: blogData },
             { new: true, upsert: true });
-        res.staus(200).send({ status: 'updated', data: updatedblog });
+        res.status(200).send({ status: 'updated', data: updatedblog });
     } catch (error) {
         res.status(500).send({ msg: error.message })
     }
@@ -75,11 +77,11 @@ const updateBlog = async function (req, res) {
 const updatingpublisherwithdate = async function (req, res) {
     try {
         let blogData = req.body;
-        let updatedblog = await blogModel.updateMany(
-            { PublishedAt: "date" },
+        let updatedblog = await blogsModel.findOneAndUpdate(
+            { publishedAt: "Jun 23 2022" },
             { $set: blogData },
-            { new: true, upsert: true });
-        res.staus(200).send({ status: 'updated', data: updatedblog });
+            { new: true, });
+        res.status(200).send({ status: 'updated', data: updatedblog });
     } catch (error) {
         res.status(500).send({ msg: error.message })
     }
@@ -90,11 +92,11 @@ const updatingpublisherwithdate = async function (req, res) {
 const deleteBlogsById = async function (req, res) {
     try {
         let blogId = req.params.blogId;
-        let result = await blogModel.findOne({ _id: blogId, isDeleted: false });
+        let result = await blogsModel.findOne({ _id: blogId, isDeleted: false });
         if (!result) return res.status(404).send({ status: false, msg: "Blog doesnot exist" });
-        let deleteblog = await blogModel.updateMany(
-            { _id: blogId },
-            { isDeleted: true, deletedAt: new Date }, { new: true });
+        let deleteblog = await blogsModel.findOneAndUpdate(
+            { _id: blogId ,isDeleted: false},
+            { $set: {isDeleted: true, deletedAt: Date.now() }}, { new: true, upsert: true }); //updatemany
         res.status(200).send({ status: true, data: deleteblog });
     } catch (err) {
         res.status(500).send({ status: false, msg: err.message });
@@ -108,7 +110,7 @@ const deleteBlogByQuerConditoin = async function (req, res) {
         const data = req.query
         if (!data) return res.status(400).send({ error: "Enter Valid Input " })
         const dataforUpdation = { ...data, isDeleted: true, isDeletedAt: new Date() }
-        const result = await blogModel.updateMany(data, dataforUpdation, { new: true })
+        const result = await blogsModel.updateMany(data, dataforUpdation, { new: true })
         if (!result) res.status(404).send({ error: "No Data Found" })
         res.status(200).send({ status: true, data: result })
     }
