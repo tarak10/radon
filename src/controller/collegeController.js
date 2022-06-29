f3003b5de44e1c7b368812a6975e75e805eead9f
rajatrout <rajatrout@Rajats-MacBook-Air.local>
2022-06-29 10:45:27 +0530

    Validation Update

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -17,18 +17,44 @@ const idV = function(value) {
 }
 
 const createCollege = async function(req, res) {
-    const { name, fullname, logoLink } = req.body
+    const { name, fullName, logoLink } = req.body
 
     if (Object.keys(req.body).length == 0) {
         resturn.status(400).send({ status: false, message: "Kindly enter your details." })
+    }
+
+    if (Object.keys(name).length == 0) {
+        return res.status(400).send({ status: false, message: "College Name is required" })
     }
 
     if (stringV(name) || idV(name)) {
         return res.status(400).send({ status: false, message: "Invalid College Name" })
     }
 
+    const duplicateName = await collegeModel.find({ name: name })
+    if (duplicateName) {
+        return res.status(400).send({ status: false, message: "The college name is already there, you can directly apply for the internship." })
+    }
+
+    if (Object.keys(fullName).length == 0) {
+        return res.status(400).send({ status: false, message: "College Name is required" })
+    }
+
     if (stringV(fullName) || idV(fullName)) {
         return res.status(400).send({ status: false, message: "Invalid Full Name of the college" })
+    }
+
+    const duplicateFullName = await collegeModel.find({ fullName: fullName })
+    if (duplicateFullName) {
+        return res.status(400).send({ status: false, message: "The college full name is already there, you can directly apply for the internship." })
+    }
+
+    if (Object.keys(logoLink).length == 0) {
+        return res.status(400).send({ status: false, message: "College logolink is required" })
+    }
+
+    if (stringV(logoLink || !validator.isURL(logoLink))) {
+        return res.status(400).send({ status: false, message: "The logoLink is not valid." })
     }
 
     let saveData = await collegeModel.create(req.body)
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,5 +1,6 @@
 const internModel = require('../model/internModel')
 const validator = require('validator')
+const collegeModel = require('../model/collegeModel')
 
 const stringV = function(value) {
     let a = typeof(value)
@@ -17,25 +18,56 @@ const idV = function(value) {
 }
 
 const createIntern = async function(req, res) {
-    const { name, email, mobile, collegeId } = req.body
+    let result = {}
+    const { name, email, mobile, collegeName } = req.body
 
     if (Object.keys(req.body).length == 0) {
         resturn.status(400).send({ status: false, message: "Kindly enter your details." })
     }
 
+    if (Object.keys(name).length == 0) {
+        return res.status(400).send({ status: false, message: "Intern name is required" })
+    }
+
     if (stringV(name) || idV(name)) {
-        return res.status(400).send({ status: false, message: "Intern name is incorrect" })
+        return res.status(400).send({ status: false, message: "Intern name is not valid" })
+    }
+    result.name = name
+
+    if (Object.keys(email).length == 0) {
+        return res.status(400).send({ status: false, message: "Intern email is required" })
     }
 
     if (stringV(email) || !validator.isEmail(email)) {
         return res.status(400).send({ status: false, message: "Intern email is incorrect" })
     }
 
+    let duplicateEmail = await internModel.find({ email: email })
+    if (duplicateEmail) {
+        return res.status(400).send({ status: false, message: "Intern email already exists." })
+    }
+    result.email = email
+
+    if (Object.keys(mobile).length == 0) {
+        return res.status(400).send({ status: false, message: "Intern mobile is required" })
+    }
+
     if ((typeof(mobile) !== Number || mobile.length < 10)) {
         return res.status(400).send({ status: false, message: "Intern mobile number is incorrect" })
     }
 
-    let saveData = await internModel.create(req.body)
+    let duplicateMobile = await internModel.find({ mobile: mobile })
+    if (duplicateMobile) {
+        return res.status(400).send({ status: false, message: "Intern mobile number already exists." })
+    }
+    result.mobile = mobile
+
+    let a = await collegeModel.find({ name: collegeName }).select({ _id: 1 })
+    if (!a) {
+        return res.status(400).send({ status: false, message: "Enter a valid college name." })
+    }
+    result.collegeId = a
+    let saveData = await (await internModel.create(rresult)).populate('collegeId')
     return res.status(201).send({ status: true, data: { saveData } })
 }
 
