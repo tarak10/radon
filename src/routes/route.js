79f8ec7b0b404fc5e4b95407a82bb68aef5e5f91
unknown <rajatkhobragade00@gmail.com>
2022-07-07 17:09:37 +0530

    d

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {createUser , userLogin,} = require("../controllers/userController")
-const {createBook,getBooks, getBooksById,deleteBookById } = require("../controllers/bookController")
+const {createBook,getBooks, getBooksById,deleteBookById,updateBooksById } = require("../controllers/bookController")
 const {auth} = require("../middleware/auth")
 
 
@@ -18,6 +18,8 @@ router.get('/books',auth, getBooks)
 router.get('/books/:bookId',auth, getBooksById)
 
 router.delete('/books/:bookId',auth,deleteBookById)
+
+router.put('/books/:bookId', updateBooksById)
 
 module.exports = router;
 
