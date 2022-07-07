df0e68c1b0fdfb1c2e5ad9e2c16b9db941c72413
ravikrpandey <104829126+ravikrpandey@users.noreply.github.com>
2022-07-07 15:24:56 +0530

    update to handel for wrong api request

--- src/routes/route.js
+++ src/routes/route.js
@@ -18,3 +18,10 @@ router.get('/books',authentication, getBooks)
 
 module.exports = router;
 
+router.all("/**", function (req, res) {
+    res.status(404).send({
+        status: false,
+        msg: "The api you request is not available"
+    })
+});
+
