c4f314f0ba312cca7f9dbe53376ed89451ca0404
Ranjeetsingh23 <Ranjeetsinghdua@gmail.com>
2022-07-07 15:58:42 +0530

    Updated

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -9,7 +9,8 @@ exports.createBook = async (req, res) => {
 
     try {
 
-        let data = req.body
+        let data = req.body;
+        let userloged = req.decodedToken //decodedToken is present in request that we have set in authorization middleware it contains loggedIn UserId
         let { title, excerpt, userId, ISBN, category, subcategory, releasedAt } = data
 
         if (Object.keys(data).length == 0)
@@ -42,18 +43,25 @@ exports.createBook = async (req, res) => {
             return res.status(400).send({ status: false, msg: "releasedAt required" })
         }
 
-        if (!/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/.test(releasedAt)) { return res.status(400).send({ status: false, msg: "Please enter date in YYYY-MM-DD" }) }
-
+       if (!/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/.test(releasedAt)) { return res.status(400).send({ status: false, msg: "Please enter date in YYYY-MM-DD" }) }
+    //    data.releasedAt = moment().format("YYYY-MM-DD,hh:mm:ss")
+    //    if(!data.releasedAt) return res.status(400).send("please send date in correct")
 
         const checkUserId = await userModel.findOne({ userId: userId })
         if (!checkUserId) { return res.status(400).send({ status: false, msg: "UserId not found" }) }
+
+      
+
 
         const checktitle = await bookModel.findOne({ title: title })
         if (checktitle) { return res.status(400).send({ status: false, msg: "title already exists please enter new title" }) }
 
         const checkIsbn = await bookModel.findOne({ ISBN: ISBN })
         if (checkIsbn) { return res.status(400).send({ status: false, msg: "ISBN already exists please enter new ISBN" }) }
-        data.releasedAt = moment().format("YYYY-MM-DD,hh:mm:ss")
+      
+        if (userId != userloged) { //In this block verifying BlogId belongs to same user or not
+            return res.status(403).send({ status: false, data: "Not authorized" })
+     }
         const saveBook = await bookModel.create(req.body)
         return res.status(201).send({ status: true, message: "Book successfully created", data: saveBook })
     }
@@ -72,11 +80,10 @@ exports.getBooks = async (req, res) => {
     try {
         let query = req.query;  //getting data from query params
 
-        // if (Object.keys(query).length == 0) {  //this block will work in case no filter is provided
-        //     const book = await bookModel.find({ isDeleted: false });
-        //     if (book.length == 0) return res.status(404).send({ status: false, msg: "No such book exist" });
-        //     return res.status(200).send({ status: true, data: book })
-        // }
+        if (Object.keys(query).length == 0) { 
+            return res.status(400).send({status:false , message:"Please provide filter in query params"})
+            
+        }
 
         let filter = {
             isDeleted: false
@@ -107,7 +114,6 @@ exports.getBooksById = async function (req, res) {
 
     try {
         let bookId = req.params.bookId;
-
         if (!validator.isValidObjectId(bookId)) return res.status(400).send({ status: false, message: "Please provide valid bookId" });
 
 
@@ -116,13 +122,12 @@ exports.getBooksById = async function (req, res) {
 
         if(book.isDeleted == true) return res.status(404).send({status: false, message:"Book is deleted or not found"})
 
-        let reviewsData = await reviewModel.find({ bookId: book._id, isDeleted: false }).select({ isDeleted: 0, __v: 0, createdAt: 0, updatedAt: 0 })
+        let reviews = await reviewModel.find({ bookId: book._id, isDeleted: false }).select({ isDeleted: 0, __v: 0, createdAt: 0, updatedAt: 0 })
          
-        // let bookData= {
-        //     book,
-        //     reviewsData
-        // }
-        return res.status(200).send({ status: true,message: "Books List", data:{ book,reviewsData} })
+        let booksWithReview = book.toObject();
+        Object.assign(booksWithReview, { reviewsData: reviews });
+        return res.status(200).send({ status: true,message: "Books List", data: booksWithReview })
+        
 
     } catch (error) {
         return res.status(500).send({ status: false, error: error.message })
@@ -136,43 +141,29 @@ exports.getBooksById = async function (req, res) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 exports.deleteBookById = async (req, res) => {
 
     try {
 
         let id = req.params.bookId;
+        let userloged = req.decodedToken //decodedToken is present in request that we have set in auth middleware it contains loggedIn UserrId
         if (!validator.isValidObjectId(id)) {
-            return res.status(400).send({ status: false, msg: `BookId is invalid.` });
+            return res.status(400).send({ status: false, msg: 'BookId is invalid.' });
         }
 
-        let Book = await bookModel.findOne({ _id: id, isDeleted: true });
-        if (!Book) {
-            return res.status(404).send({ status: false, msg: "No such Book found" });
-        }
+        if (id) {
+            let findbook = await bookModel.findById(id)
+            if (!findbook) return res.status(404).send({ status: false, msg: `no book found by this BookID:${id}` });
 
-        if (Book.isDeleted == false) {
+            if (findbook.userId != userloged) {
+                return res.status(403).send({ status: false, data: "Not authorized" })
+            }
+
+        if (findbook.isDeleted == false) {
             let Update = await bookModel.findOneAndUpdate(
                 { _id: id },
-                { isDeleted: true, deletedAt: Date() },
-                { new: true });
+                { isDeleted: true, deletedAt: new Date() 
+                });
             return res.status(200).send({ status: true, msg: "Your data deleted successfully" });
 
         } else {
@@ -180,7 +171,7 @@ exports.deleteBookById = async (req, res) => {
                 .status(404)
                 .send({ status: false, msg: "Book already deleted" });
         }
-    } catch (err) {
+    } }catch (err) {
         res.status(500).send({ status: false, msg: err.message });
     }
 }
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,25 +1,23 @@
 const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const userModel = require("../models/userModel")
-const { isValidObjectId } = require('../validator/validator')
-exports.authentication = async (req, res, next) => {
 
-    try {
-        let token = req.headers['X-API-KEY'];
-        if (!token) {
-            token = req.headers['x-api-key'];
-        }
-        if (!token) return res.status(400).send({ status: false, message: "Token is missing" });
+exports.auth = async (req, res, next) => {
 
-        let decodedToken = jwt.verify(token, "lama");
-        if (!decodedToken) return res.status(401).send({ status: false, message: "Token is not valid" })
-        req.decodedToken = decodedToken;
-        next();
-    } catch (err) { //hardcoded them
-        if (err.message == "jwt expired") return res.status(400).send({ status: false, message: "JWT expired, login again" })
-        if (err.message == "invalid signature") return res.status(400).send({ status: false, message: "Token is incorrect" })
-      return  res.status(500).send({ status: false, error: err.message })
+  try {
+    let token = req.headers['X-API-KEY'];
+    if (!token) {
+      token = req.headers['x-api-key'];
     }
+    if (!token) return res.status(400).send({ status: false, message: "Token is missing" });
+
+    let decodedToken = jwt.verify(token, "lama");
+    if (!decodedToken) return res.status(401).send({ status: false, message: "Token is not valid" })
+    req.decodedToken = decodedToken.userId;
+    next();
+  } catch (err) { //hardcoded them
+    if (err.message == "jwt expired") return res.status(400).send({ status: false, message: "JWT expired, login again" })
+    if (err.message == "invalid signature") return res.status(400).send({ status: false, message: "Token is incorrect" })
+    return res.status(500).send({ status: false, error: err.message })
+  }
 
 }
 
@@ -30,27 +28,3 @@ exports.authentication = async (req, res, next) => {
 
 
 
-
-exports.authorize = async function (req, res, next) {
-
-    try {
-
-        const userId = req.body.userId
-        let userlogging= req.decodedToken.userId
-        let loggedinUser;
-
-        // if (!isValidObjectId(userId)) {
-        //     return res.status(403).send("Provide valid userId")
-        // }
-
-        const data = await userModel.findById(userId)
-        if (!data) return res.status(404).send("UserId not found ")
-        loggedinUser = data._id;
-
-        if(userlogging != loggedinUser)
-        return res.status(403).send({status: false , message :"Unauthorized to access"})
-        next()
-    } catch (error) {
-       return  res.status(500).send({ status: false, error: error.message })
-    }
-    }
