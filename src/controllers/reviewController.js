c78dea273e7c773da08d89d59c73f688c7da92e6
Ranjeetsingh23 <Ranjeetsinghdua@gmail.com>
2022-07-11 13:07:28 +0530

    Updated

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -40,13 +40,10 @@ exports.createReview = async (req, res) => {
   } catch (error) {
     return res.status(500).send({ status: false, error: error.message })
   }
-
-
-
 }
 
-exports.updateReview = async (req,res) => {
-  try{
+exports.updateReview = async (req, res) => {
+  try {
 
     let id = req.params;
     let { bookId, reviewId } = id;
@@ -63,19 +60,31 @@ exports.updateReview = async (req,res) => {
     let checkReview = await reviewModel.findById(reviewId);
     if (!checkReview) return res.status(404).send({ status: false, message: "Review not found" });
 
-    if(checkReview.bookId.toString() !== bookId) return res.status(400).send({status: false, message: "Please enter correct bookId"})
+    if (checkReview.bookId.toString() !== bookId) return res.status(400).send({ status: false, message: "Please enter correct bookId" })
 
     if (checkReview.isDeleted == true) return res.status(404).send({ status: false, message: "Review is already deleted" });
-     
+
     let data = req.body;
 
-    let { review, rating, reviewedBy} = data;
+    let { review, rating, reviewedBy } = data;
 
-    
+    if (!rating) return res.status(400).send({ status: false, message: "Rating is required and should not be zero" });
 
+    if (validator.validString(reviewedBy) || validator.validString(review)) return res.status(400).send({ status: false, send: "Please enter rewiew and reviewdBy in a valid string" });
 
-  }catch(error){
-    return res.status(500).send({status: false , message: error.message})
+    if (!validator.validString(rating)) return res.status(400).send({ status: false, message: "Please enter rating in digits" });
+
+    if (!((rating < 6) && (rating > 0))) return res.status(400).send({ status: false, message: "Rating should be between 1 - 5 numbers" });
+
+    let updateReview = await reviewModel.findByIdAndUpdate(
+      { _id: reviewId },
+      data,
+      { new: true }
+    )
+
+    return res.status(200).send({ status: true, message: "Review Updated Successfully", data: updateReview })
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error.message })
   }
 }
 
@@ -97,7 +106,7 @@ exports.deleteReview = async (req, res) => {
     let review = await reviewModel.findById(reviewId);
     if (!review) return res.status(404).send({ status: false, message: "Review not found" });
 
-    if(review.bookId.toString() !== bookId) return res.status(400).send({status: false, message: "Please enter correct bookId"})
+    if (review.bookId.toString() !== bookId) return res.status(400).send({ status: false, message: "Please enter correct bookId" })
 
     if (review.isDeleted == true) return res.status(404).send({ status: false, message: "Review is already deleted" });
 
