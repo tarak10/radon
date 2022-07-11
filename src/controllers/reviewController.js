e744b4b5453f17c2a3ac46cb9b523e1d7147cb6b
Ranjeetsingh23 <Ranjeetsinghdua@gmail.com>
2022-07-11 11:28:21 +0530

    Updated

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -3,42 +3,44 @@ const bookModel = require('../models/bookModel')
 const validator = require('../validator/validator')
 
 
-exports.createReview = async (req,res) => {
- try{
+exports.createReview = async (req, res) => {
+  try {
     let id = req.params.bookId
 
-    if(!validator.isValidObjectId(id)) return res.status(400).send({ status:false , message: "Please enter valid bookId"});
+    if (!validator.isValidObjectId(id)) return res.status(400).send({ status: false, message: "Please enter valid bookId" });
 
     let checkBook = await bookModel.findById(id);
-    if(!checkBook) return res.status(404).send({ status: false , message: "Book not found"});
-     
-    if(checkBook.isDeleted == true) return res.status(404).send({status: false , message : "Book is already deleted"});
+    if (!checkBook) return res.status(404).send({ status: false, message: "Book not found" });
+
+    if (checkBook.isDeleted == true) return res.status(404).send({ status: false, message: "Book is already deleted" });
 
     let data = req.body;
 
-    if(Object.keys(data).length == 0) return res.status(400).send({status: false, message: "Please provide data in body"});
+    if (Object.keys(data).length == 0) return res.status(400).send({ status: false, message: "Please provide data in body" });
 
-    let {review, rating, reviewedBy} = data;
+    let { review, rating, reviewedBy } = data;
 
-    if(!rating) return res.status(400).send({status: false , message: "Rating is required and should not be zero"});
+    if (!rating) return res.status(400).send({ status: false, message: "Rating is required and should not be zero" });
 
-    if(validator.validString(reviewedBy) || validator.validString(review)) return res.status(400).send({status: false , send: "Please enter rewiew and reviewdBy in a valid string"});
+    if (validator.validString(reviewedBy) || validator.validString(review)) return res.status(400).send({ status: false, send: "Please enter rewiew and reviewdBy in a valid string" });
 
-    if(!validator.validString(rating)) return res.status(400).send({status: false , message: "Please enter rating in digits"});
+    if (!validator.validString(rating)) return res.status(400).send({ status: false, message: "Please enter rating in digits" });
 
-    if(!((rating < 6 ) && (rating > 0))) return res.status(400).send({ status: false, message: "Rating should be between 1 - 5 numbers" });
-    
+    if (!((rating < 6) && (rating > 0))) return res.status(400).send({ status: false, message: "Rating should be between 1 - 5 numbers" });
+
     data.bookId = id //storing bookId got from params inside reviews bookId
-    let reviewData = await reviewModel.create(data)
-                  await bookModel.updateOne(
-                    {_id:id},
-                    {$inc: {reviews: 1}}
-                  )
-                  return res.status(200).send({status: true , message: "Success", data: reviewData})
-     }catch(error){
-         return res.status(500).send({status: false , error: error.message})
-     }
 
-  
+    let reviewData = await reviewModel.create(data)
+    
+    await bookModel.updateOne(
+      { _id: id },
+      { $inc: { reviews: 1 } }
+    )
+    return res.status(200).send({ status: true, message: "Success", data: reviewData })
+  } catch (error) {
+    return res.status(500).send({ status: false, error: error.message })
+  }
+
+
 
 }
